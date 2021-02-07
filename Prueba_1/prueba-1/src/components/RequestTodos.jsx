55d96a82228905ba330905822da0f3b02b9e655b
Santiago Serrano Márquez <santiago.serrano.marquez@hotmail.com>
2021-02-06 20:05:13 -0600

    Minor fixes

--- Prueba_1/prueba-1/src/components/RequestTodos.jsx
+++ Prueba_1/prueba-1/src/components/RequestTodos.jsx
@@ -90,7 +90,7 @@ function RequestTodos(){
                                     <Todos { ...rqt } />
                                 </Grid>
                             </>
-                        )
+                        ).reverse()
             }
         </>
     )
